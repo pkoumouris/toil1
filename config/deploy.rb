366270954afe212dcb78fe8de6b929ce4acab4a1
Parris Koumouris <parris.koumouris@acl.org.au>
2023-01-25 12:16:04 +1100

    Configure Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.1"
 
 set :application, "toil1"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:pkoumouris/toil1.git"
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
