ed4656fd322a5caf30dfb34ba8ffa97a3f376359
Parris Koumouris <parris.koumouris@acl.org.au>
2023-01-31 10:26:49 +1100

    Try prerender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ###
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 end
