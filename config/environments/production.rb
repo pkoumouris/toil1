46ed20c9730e79c3a6575a4b1b18c8e530f1371c
Parris Koumouris <parris.koumouris@acl.org.au>
2023-01-31 11:56:51 +1100

    mountComponents() earlier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,5 +124,5 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 end
